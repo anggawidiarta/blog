d4587ae9f3ca5ff68b504067fcc144a8921e96ac
anggawidiarta <anggawidiarta55@gmail.com>
2023-12-21 23:53:02 +0800

    upload blog

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+  site_name: 'Astro boilerplate',
+  title: 'Sloka',
+  description: 'Boilerplate built with Astro using React and Tailwind CSS',
+  author: 'Angga Widiarta',
+  locale_region: 'en-us',
+  locale: 'en',
+};
